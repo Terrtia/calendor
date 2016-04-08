2155ce3a65ed8f02459dae529f0b146e304299c0
Terrtia <or1994@hotmail.fr>
2016-04-08 10:46:23 +0200

    mise en forme html

--- HtmlGenerator.php
+++ HtmlGenerator.php
@@ -8,6 +8,15 @@
 class HtmlGenerator {
     
     public $modele;
+    public $debutEntete = '<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Salle de Réunion</title>';
+            
+    public $finEntete = "\n    </head>\n";
+    public $debutBody = "\n    <body>\n";
+    public $finBody = "    </body>\n</html>";
     
     public function __construct($modele) {
         $this->modele = $modele;
@@ -23,9 +32,16 @@ class HtmlGenerator {
             unlink($file);
         }
         
+        //contenu du fichier :
+            //entete
+        $html = $this->debutEntete . $this->finEntete;
+            //body
+        $html .= $this->debutBody . $this->finBody; 
+        
+        
         //creation du fichier
         $myfile = fopen($file, "w") or die("Unable to open/create calendrier.html");
-        
+        fwrite($myfile, $html);
         
         //fermeture fichier
         fclose($myfile);
