77006bfb2114104155153251bde0a4d369c0040b
Terrtia <or1994@hotmail.fr>
2016-04-08 16:57:00 +0200

    debut traitement ics

--- ICalReader.php
+++ ICalReader.php
@@ -9,6 +9,7 @@ class ICalReader {
     
     public $modele;
     public $nb_fichier;
+    public $tableauFichier;
     
     
     public function __construct($modele) {
@@ -18,15 +19,45 @@ class ICalReader {
     
     public function readFiles() {
         
+        //Expressions
+        $ExpDateDebut = 'DTSTART:(.*)';
+        $ExpDateFin = 'DTEND;TZID="Romance Standard Time":(.*)';
+        $ExpOrganisateur = 'ORGANIZER;CN=(.*)';
+        $ExpLieu = 'LOCALISATION:(.*)';
+        $ExpDescription = 'SUMMARY;LANGUAGE=fr:(.*)';
+        
         if($dossier = opendir('./ics')){
             
             while( ($fichier = readdir($dossier)) !== FALSE){
                 if($fichier != '.' && $fichier != '..'){
-                    
+                 
                     $this->nb_fichier++;
                     echo $fichier . '  ';
                     
                     //creation des salles avec les infos ici
+                    //ouverture du fichier .ics
+                    if($fichierIcs = fopen("ics/" . $fichier, "r")){
+                        
+                        $cal=fread($fichierIcs, filesize("ics/" . $fichier) );
+                        
+                        $n = preg_match_all($ExpDescription, $cal, $descriptionTab, PREG_PATTERN_ORDER);
+                        preg_match_all($ExpDateDebut, $cal, $debutTab, PREG_PATTERN_ORDER);
+                        preg_match_all($ExpDateFin, $cal, $finTab, PREG_PATTERN_ORDER);
+                        preg_match_all($ExpOrganisateur, $cal, $OrganisateurTab, PREG_PATTERN_ORDER);
+                        preg_match_all($ExpLieu, $cal, $LieuTab, PREG_PATTERN_ORDER);
+                        
+                        //Recuperation des donnees des reunions
+                        for($i = 0; $i<$n; $i++){
+                            
+                        }
+                        
+                        
+                    } else {
+                        echo "Erreur lecture fichier ics : " . $fichier ;
+                    }
+                    
+                    //fermeture du fichier .ics
+                    fclose($fichierIcs);
                     
                 }
             }
@@ -38,6 +69,9 @@ class ICalReader {
         }
         
         echo'fin lecture du repertoire, nombre de fichier = ' . $this->nb_fichier;
+        
+        
+        
     }
     
     
